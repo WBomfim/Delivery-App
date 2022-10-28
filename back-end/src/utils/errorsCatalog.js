05a488735f126d3581b25d7070be3935965f653f
WBomfim <willianbomfim@hotmail.com>
2022-10-28 19:06:54 -0300

    feat: Criado o middleware para validação do token dos usuários

--- back-end/src/utils/errorsCatalog.js
+++ back-end/src/utils/errorsCatalog.js
@@ -3,6 +3,8 @@ const statusHttp = require('./statusHttp');
 const errorsTypes = {
   USER_NOT_FOUND: 'USER_NOT_FOUND',
   INVALID_PASSWORD: 'INVALID_PASSWORD',
+  TOKEN_NOT_FOUND: 'TOKEN_NOT_FOUND',
+  INVALID_TOKEN: 'INVALID_TOKEN',
 };
 
 const errorsCatalog = {
@@ -13,6 +15,14 @@ const errorsCatalog = {
   INVALID_PASSWORD: {
     statusHttp: statusHttp.UNAUTHORIZED,
     error: 'Invalid password',
+  },
+  TOKEN_NOT_FOUND: {
+    statusHttp: statusHttp.UNAUTHORIZED,
+    error: 'Token not found',
+  },
+  INVALID_TOKEN: {
+    statusHttp: statusHttp.UNAUTHORIZED,
+    error: 'Expired or invalid token',
   },
 };
 
