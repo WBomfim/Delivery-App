af810d7a1cfa45174733c546774f00da8d19feba
WBomfim <willianbomfim@hotmail.com>
2022-10-28 16:31:43 -0300

    feat: Criado a rota POST /login - Essa rota valida o usuário e retorna um object com um token para validações futuras

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 require('express-async-errors');
+const loginRoutes = require('../routes/login');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/coffeee', (_req, res) => res.status(418).end());
+
+app.use('/login', loginRoutes);
 
 module.exports = app;
