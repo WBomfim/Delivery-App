9660587a55b51a1f29feab6fe8f992e9c38d8dfe
WBomfim <willianbomfim@hotmail.com>
2022-10-28 13:43:15 -0300

    feat: Criado um schema para validar as informações de login

---
+++ back-end/src/schemas/validateLoginInfos.js
@@ -0,0 +1,30 @@
+const joi = require('joi');
+
+const schemaEmail = joi.email().empty().required().messages({
+  'string.empty': '400|The "email" field is required',
+  'any.required': '400|email is required',
+});
+
+const schemaPassword = joi.string().min(6).empty().required()
+  .messages({
+  'string.empty': '400|The "password" field is required',
+  'any.required': '400|password is required',
+  'string.min': '400|password must be at least 6 characters',
+});
+
+const validateLoginInfos = async ({ email, password }) => {
+  const schema = joi.object().keys({
+    email: schemaEmail,
+    password: schemaPassword,
+  });
+
+  const { error } = schema.validate({ email, password });
+  if (error) {
+    const [code, message] = error.message.split('|');
+    throw new Error({ code: Number(code), error: { message } });
+  }
+
+  return true;
+};
+
+module.exports = validateLoginInfos;
