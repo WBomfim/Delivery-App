1f941a367e05956d7eacf7f0ac54375a3c9e405a
WBomfim <willianbomfim@hotmail.com>
2022-11-01 01:27:22 -0300

    feat: Criado o model saleProduct

---
+++ back-end/src/database/models/saleProduct.js
@@ -0,0 +1,40 @@
+const saleProduct = (sequelize, DataTypes) => {
+  const saleProduct = sequelize.define('saleProduct', {
+      sale_id: {
+        primaryKey: true,
+        foreignKey: true,
+        type: DataTypes.INTEGER,
+      },
+      product_id: {
+        primaryKey: true,
+        foreignKey: true,
+        type: DataTypes.INTEGER,
+      },
+      quantity: DataTypes.INTEGER,
+    },
+    { 
+      timestamps: false,
+      underscored: true,
+    },
+  );
+
+  saleProduct.associate = (models) => {
+    models.sale.belongsToMany(models.product, {
+      through: models.saleProduct,
+      foreignKey: 'sale_id',
+      otherKey: 'product_id',
+      as: 'products',
+    });
+
+    models.product.belongsToMany(models.sale, {
+      through: models.saleProduct,
+      foreignKey: 'product_id',
+      otherKey: 'sale_id',
+      as: 'sales',
+    });
+  };
+
+  return saleProduct;
+};
+
+module.exports = saleProduct;
