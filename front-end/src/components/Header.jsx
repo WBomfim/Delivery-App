7dbeb3e516f947d2e0e6cacb814309aa972a4f15
Fernando Rocha <fernandodasilvarocha2@gmail.com>
2022-11-03 06:00:03 -0300

    feat: tela de produtos 100% finalizada

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,28 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Header() {
+  const navigate = useNavigate();
+  const [nameUser, setNameUser] = useState('');
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('user'));
+    if (!user) {
+      setNameUser('Ciclano da Silva');
+    } else {
+      setNameUser(user.name);
+    }
+  }, []);
+
+  const logoutBtn = () => {
+    localStorage.clear();
+    navigate('/');
+  };
+
+  const redirectProdutos = () => navigate('/customer/products');
+
+  const redirectMeusPedidos = () => navigate('/customer/orders');
+
   return (
     <section>
       <button
         type="button"
         data-testid="customer_products__element-navbar-link-products"
+        onClick={ redirectProdutos }
       >
         PRODUTOS
       </button>
       <button
         type="button"
         data-testid="customer_products__element-navbar-link-orders"
+        onClick={ redirectMeusPedidos }
       >
         MEUS PEDIDOS
       </button>
       <p
         data-testid="customer_products__element-navbar-user-full-name"
       >
-        Ciclano da Silva
+        { nameUser }
       </p>
       <button
         type="button"
         data-testid="customer_products__element-navbar-link-logout"
+        onClick={ logoutBtn }
       >
         Sair
       </button>
