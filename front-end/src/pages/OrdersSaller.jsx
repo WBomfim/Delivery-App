f21235021daa2ad1a7f289c0296b5a21f12f1460
WBomfim <willianbomfim@hotmail.com>
2022-11-02 21:01:36 -0300

    feat: Criado a pagina de pedidos do vendedor

--- front-end/src/pages/OrdersSaller.jsx
+++ front-end/src/pages/OrdersSaller.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
+import CardSale from '../components/CardSale';
+import { getData } from '../services/requests';
 
 export default function OrdersSaller() {
+  const [sales, setSales] = useState([]);
+
+  useEffect(() => {
+    const getSales = async () => {
+      const data = await getData('/sales');
+      setSales(data);
+    };
+    getSales();
+  }, []);
+
   return (
     <>
       <Header />
-      <div>
-        <h2>Testeeeeeee</h2>
-      </div>
+      <section>
+        {sales.map((sale) => (
+          <CardSale key={ sale.id } sale={ sale } />
+        ))}
+      </section>
     </>
   );
 }
