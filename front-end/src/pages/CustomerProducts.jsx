14c34b4ce4a5d8cf602b979287907c57cd51c888
WBomfim <willianbomfim@hotmail.com>
2022-11-10 17:18:19 -0300

    refactor: Refatorado o a página CustomerProducts - Foram implementadas as novas funções para requests e informações do local storage, também foi revisado a semantica do html e a lógica da requisição das informações.

--- front-end/src/pages/CustomerProducts.jsx
+++ front-end/src/pages/CustomerProducts.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { setToken, requestData } from '../services/requests';
+import { logout } from '../services/handleStorage';
 import Header from '../components/Header';
 import CardProducts from '../components/CardProduct';
 import DeliveryContext from '../context/DeliveryContext';
-import { logout } from '../services/handleStorage';
 
-export default function ProdutosClientes() {
+export default function CustomerProducts() {
   const [products, setProducts] = useState([]);
   const { totalValue } = useContext(DeliveryContext);
   const navigate = useNavigate();
@@ -26,32 +26,32 @@ export default function ProdutosClientes() {
   }, [navigate]);
 
   return (
-    <section>
+    <>
       <Header />
-      { products.map((ev, i) => (
-        <CardProducts
-          key={ i }
-          id={ ev.id }
-          name={ ev.name }
-          price={ Number(ev.price) }
-          urlImage={ ev.urlImage }
-        />
-      )) }
+      <main>
+        {
+          products.map((ev, i) => (
+            <CardProducts
+              key={ i }
+              id={ ev.id }
+              name={ ev.name }
+              price={ Number(ev.price) }
+              urlImage={ ev.urlImage }
+            />
+          ))
+        }
 
-      <button
-        disabled={ totalValue === 0 }
-        onClick={ () => navigate('/customer/checkout') }
-        type="button"
-        data-testid="customer_products__button-cart"
-      >
         <button
           type="button"
+          disabled={ totalValue === 0 }
           onClick={ () => navigate('/customer/checkout') }
-          data-testid="customer_products__checkout-bottom-value"
+          data-testid="customer_products__button-cart"
         >
-          { totalValue.toFixed(2).toString().replace('.', ',') }
+          <div data-testid="customer_products__checkout-bottom-value">
+            { `Ver Carrinho: R$ ${totalValue.toFixed(2).toString().replace('.', ',')}` }
+          </div>
         </button>
-      </button>
-    </section>
+      </main>
+    </>
   );
 }
