32412265cdcab94fb15e20d685f1c605809c3bcc
WBomfim <willianbomfim@hotmail.com>
2022-11-10 17:58:19 -0300

    chore: Revisado a semântica do html e o nome do componente CardProduct

--- front-end/src/components/CardProduct.jsx
+++ front-end/src/components/CardProduct.jsx
@@ -2,36 +2,39 @@ import React, { useState, useContext, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import DeliveryContext from '../context/DeliveryContext';
 
-export default function CardProducts({ id, name, price, urlImage }) {
+export default function CardProduct({ id, name, price, urlImage }) {
   const [quantyProduct, setquantyProduct] = useState(0);
   const { productsCarShop, setProductsCarShop } = useContext(DeliveryContext);
 
   useEffect(() => {
-    if (quantyProduct > 0) {
-      const verificaProduct = productsCarShop.some((ev) => ev.productId === id);
-      if (verificaProduct) {
-        setProductsCarShop(productsCarShop.map((ev) => {
-          if (ev.productId === id) {
-            ev.quantity = quantyProduct;
-            ev.totalPrice = quantyProduct * ev.unitPrice;
-          }
-          return ev;
-        }));
+    const handleCarShop = () => {
+      if (quantyProduct > 0) {
+        const verificaProduct = productsCarShop.some((ev) => ev.productId === id);
+        if (verificaProduct) {
+          setProductsCarShop(productsCarShop.map((ev) => {
+            if (ev.productId === id) {
+              ev.quantity = quantyProduct;
+              ev.totalPrice = quantyProduct * ev.unitPrice;
+            }
+            return ev;
+          }));
+        } else {
+          setProductsCarShop([...productsCarShop, {
+            productId: id,
+            nameProduct: name,
+            unitPrice: price,
+            quantity: quantyProduct,
+            totalPrice: quantyProduct * Number(price),
+          }]);
+        }
       } else {
-        setProductsCarShop([...productsCarShop, {
-          productId: id,
-          nameProduct: name,
-          unitPrice: price,
-          quantity: quantyProduct,
-          totalPrice: quantyProduct * Number(price),
-        }]);
+        const verificaProduct = productsCarShop.some((ev) => ev.productId === id);
+        if (verificaProduct) {
+          setProductsCarShop(productsCarShop.filter((ev) => ev.productId !== id));
+        }
       }
-    } else {
-      const verificaProduct = productsCarShop.some((ev) => ev.productId === id);
-      if (verificaProduct) {
-        setProductsCarShop(productsCarShop.filter((ev) => ev.productId !== id));
-      }
-    }
+    };
+    handleCarShop();
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [quantyProduct]);
 
@@ -45,52 +48,54 @@ export default function CardProducts({ id, name, price, urlImage }) {
     setquantyProduct(quantyProduct + 1);
   };
 
-  const digitarQuanty = ({ target }) => {
+  const setQuantity = ({ target }) => {
     setquantyProduct(Number(target.value));
   };
 
   return (
     <div>
-      <section data-testid={ `customer_products__element-card-price-${id}` }>
-        {/* referencia site https://www.devmedia.com.br/forum/trocar-ponto-por-virgula-javascript/551468#:~:text=Para%20isso%2C%20altere%20utilize%20o,toString(). */}
-        { price.toFixed(2).toString().replace('.', ',') }
-      </section>
-      <section>
+      <div>
+        <p data-testid={ `customer_products__element-card-price-${id}` }>
+          { price.toFixed(2).replace('.', ',') }
+        </p>
         <img
+          width="120px"
           src={ urlImage }
           alt={ name }
           data-testid={ `customer_products__img-card-bg-image-${id}` }
         />
-      </section>
-      <section data-testid={ `customer_products__element-card-title-${id}` }>
-        { name }
-      </section>
-      <section>
-        <button
-          data-testid={ `customer_products__button-card-rm-item-${id}` }
-          type="button"
-          onClick={ removeItem }
-        >
-          -
-        </button>
-        <input
-          value={ quantyProduct }
-          onChange={ digitarQuanty }
-          data-testid={ `customer_products__input-card-quantity-${id}` }
-        />
-        <button
-          data-testid={ `customer_products__button-card-add-item-${id}` }
-          type="button"
-          onClick={ addItem }
-        >
-          +
-        </button>
-      </section>
+      </div>
+      <div>
+        <p data-testid={ `customer_products__element-card-title-${id}` }>
+          { name }
+        </p>
+        <div>
+          <button
+            type="button"
+            onClick={ removeItem }
+            data-testid={ `customer_products__button-card-rm-item-${id}` }
+          >
+            -
+          </button>
+          <input
+            value={ quantyProduct }
+            onChange={ setQuantity }
+            data-testid={ `customer_products__input-card-quantity-${id}` }
+          />
+          <button
+            type="button"
+            onClick={ addItem }
+            data-testid={ `customer_products__button-card-add-item-${id}` }
+          >
+            +
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
 
-CardProducts.propTypes = {
+CardProduct.propTypes = {
   id: PropTypes.number,
   name: PropTypes.string,
   price: PropTypes.number,
--- front-end/src/pages/CustomerProducts.jsx
+++ front-end/src/pages/CustomerProducts.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { setToken, requestData } from '../services/requests';
 import { logout } from '../services/handleStorage';
 import Header from '../components/Header';
-import CardProducts from '../components/CardProduct';
+import CardProduct from '../components/CardProduct';
 import DeliveryContext from '../context/DeliveryContext';
 
 export default function CustomerProducts() {
@@ -31,7 +31,7 @@ export default function CustomerProducts() {
       <main>
         {
           products.map((ev, i) => (
-            <CardProducts
+            <CardProduct
               key={ i }
               id={ ev.id }
               name={ ev.name }
